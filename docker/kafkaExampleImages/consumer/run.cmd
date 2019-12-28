beb58efdc43af3c33f94131bcb04fcf3525c078f
JacobSogaard <sogaard1995@gmail.com>
2019-12-28 14:16:36 +0100

    Removed flume and kafka examples

--- docker/kafkaExampleImages/consumer/run.cmd
+++
@@ -1,2 +0,0 @@
-docker build . -t consumerexample:latest 
-docker run --rm --ip 172.200.1.240 --hostname consumer --network kafkaNetwork consumerexample:latest
--- docker/kafkaExampleImages/producer/run.cmd
+++
@@ -1,2 +0,0 @@
-docker build . -t producerexample:latest 
-docker run --rm --ip 172.200.1.239 --hostname producer --network kafkaNetwork producerexample:latest
