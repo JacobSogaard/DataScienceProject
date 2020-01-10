f1d8248fab261f5b6c89f6223ee8828ceb594579
Jonas Mohr Pedersen <mohrdevelopment@gmail.com>
2020-01-10 22:57:39 +0100

    think i fixed startup procedure

--- postgresql/init.sql
+++ postgresql/init.sql
@@ -1,8 +1,3 @@
-CREATE TABLE cities (
-  name varchar(80),
-  location point
-);
-
 CREATE TABLE clustercenters (
   id SERIAL PRIMARY KEY,
   coordinates float[]
@@ -15,17 +10,4 @@ CREATE TABLE kmeansoutput (
   percent float,
   cluster integer REFERENCES clustercenters(id)
 );
-
-INSERT INTO cities VALUES('Copenhagen', '(-119.0, 53.0)');
-INSERT INTO cities VALUES('San Francisco', '(-119.0, 53.0)');
-INSERT INTO cities VALUES('Oslo', '(-119.0, 53.0)');
-INSERT INTO cities VALUES('Barcelona', '(-119.0, 53.0)');
-INSERT INTO cities VALUES('London', '(-119.0, 53.0)');
-INSERT INTO cities VALUES('Sofia', '(-119.0, 53.0)');
-INSERT INTO cities VALUES('Prague', '(-119.0, 53.0)');
-INSERT INTO cities VALUES('Berlin', '(-119.0, 53.0)');
-INSERT INTO cities VALUES('Bern', '(-119.0, 53.0)');
-INSERT INTO cities VALUES('Paris', '(-119.0, 53.0)');
-INSERT INTO cities VALUES('Washington', '(-119.0, 53.0)');
-
 
